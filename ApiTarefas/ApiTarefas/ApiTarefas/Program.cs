2e968bebf51ccac2c2a8bf9525a9885eeec31514
Diogo <cfeld.diogo@gmail.com>
2023-04-03 18:57:33 -0300

    min p2

--- ApiTarefas/ApiTarefas/ApiTarefas/Program.cs
+++ ApiTarefas/ApiTarefas/ApiTarefas/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+   opt.UseInMemoryDatabase("TarefasDB"));
+
 
 var app = builder.Build();
 
@@ -17,3 +24,23 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+
+
+
+
+
+class Tarefa
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool IsConcluida { get; set; }
+}
+
+class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    { }
+
+    public DbSet<Tarefa> Tarefas => Set<Tarefa>();
+}
