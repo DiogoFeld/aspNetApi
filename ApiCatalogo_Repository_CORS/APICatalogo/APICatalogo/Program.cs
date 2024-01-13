ca76f386e948276a8f594f99c7b0acbb4290c0a8
Diogo Feld <cfeld.diogo@gmail.com>
2024-01-13 00:25:16 -0300

    projeto CORS adicionado

---
+++ ApiCatalogo_Repository_CORS/APICatalogo/APICatalogo/Program.cs
@@ -0,0 +1,98 @@
+using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repository;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container. // => ConfigureServices
+builder.Services.AddControllers()
+      .AddJsonOptions(options =>
+         options.JsonSerializerOptions
+            .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseMySql(mySqlConnection,
+                    ServerVersion.AutoDetect(mySqlConnection)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+
+//JWT
+//adiciona o manipulador de autenticacao e define o 
+//esquema de autenticacao usado : Bearer
+//valida o emissor, a audiencia e a chave
+//usando a chave secreta valida a assinatura
+builder.Services.AddAuthentication(
+    JwtBearerDefaults.AuthenticationScheme).
+    AddJwtBearer(options =>
+     options.TokenValidationParameters = new TokenValidationParameters
+     {
+         ValidateIssuer = true,
+         ValidateAudience = true,
+         ValidateLifetime = true,
+         ValidAudience = builder.Configuration["TokenConfiguration:Audience"],
+         ValidIssuer = builder.Configuration["TokenConfiguration:Issuer"],
+         ValidateIssuerSigningKey = true,
+         IssuerSigningKey = new SymmetricSecurityKey(
+             Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
+     });
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+var mappingConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingProfile());
+});
+
+IMapper mapper = mappingConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+////definir politica cors via atributo
+//builder.Services.AddCors(options =>
+//{
+//    options.AddPolicy("PermitirApiRequest",
+//        builder =>
+//        builder.WithOrigins("https://wwwapirequest.io/")
+//     .WithMethods("GET")
+//     );
+//});
+builder.Services.AddCors();
+
+
+var app = builder.Build();// Configure
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+//politica CORS restritiva
+//app.UseCors(opt => opt.
+//    WithOrigins("https://wwwapirequest.io/")
+//     .WithMethods("GET"));
+
+app.UseCors(opt => opt.AllowAnyOrigin());
+
+app.MapControllers();
+app.Run();
