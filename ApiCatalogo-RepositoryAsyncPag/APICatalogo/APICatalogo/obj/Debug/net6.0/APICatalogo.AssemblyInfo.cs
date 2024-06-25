b0c16158fa444c9aafa77d38b1d381bd2ddc91e2
Diogo Feld <cfeld.diogo@gmail.com>
2024-06-24 23:00:45 -0300

    Send To GitIgnore

--- ApiCatalogo-RepositoryAsyncPag/APICatalogo/APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
+++ ApiCatalogo-RepositoryAsyncPag/APICatalogo/APICatalogo/obj/Debug/net6.0/APICatalogo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff68c13169bdfec14c11749cf4e1905c06d07d33")]
 [assembly: System.Reflection.AssemblyProductAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APICatalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
